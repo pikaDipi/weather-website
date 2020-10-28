3aea07db4b23c72df414ee40340cfdb5b25e4cea
Dipika <dpksingh101996@gmail.com>
2020-10-28 16:03:54 +0530

    Feature 1 included

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -5,6 +5,7 @@ const url ='http://api.weatherstack.com/current?access_key=0bd33409b21af42b14b4a
 request({ url: url , json: true }, (error, { body }) => {
        if(error)
        {
+           //Code change for feature 1
            callback('Unable to connect to weather service',undefined)
        } else if(body.error) {
            callback('Unable to find location.')
