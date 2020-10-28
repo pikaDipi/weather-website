f21ccee2af80efb099b7998686aefae65015fa39
Dipika <dpksingh101996@gmail.com>
2020-10-28 17:42:50 +0530

    Add humidity and precip to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ request({ url: url , json: true }, (error, { body }) => {
        } else if(body.error) {
            callback('Unable to find location.')
        } else{
-           callback(undefined, body.current.weather_descriptions[0] +' : It is currently ' +body.current.temperature +' degrees out. But it feels like '+body.current.feelslike +' degrees.')
+           callback(undefined, body.current.weather_descriptions[0] +' : It is currently ' +body.current.temperature +' degrees out. But it feels like '+body.current.feelslike +' degrees if the humidity is '+body.current.humidity +' %. And there is '+body.current.precip +'% chance of rain.' )
        }
     })
 }   
